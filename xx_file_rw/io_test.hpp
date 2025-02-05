f05aea35558ca015194e1e555c7cb58120e07ff0
xjp <qt00@qq.com>
2025-02-05 14:50:11 +0800

    test

--- xx_file_rw/io_test.hpp
+++ xx_file_rw/io_test.hpp
@@ -3,34 +3,197 @@
 
 
 
-static void io_test2()
-{
-    io_context ctx;
+// static void io_test2()
+// {
+//     io_context ctx;
     
-    io i;
-    i.init(ctx);
+//     io i;
+//     i.init(ctx);
 
-    for (size_t idx = 0; idx < 2000; idx++)
-    {
-        i.idx_.add(idx);
-    }
+//     for (size_t idx = 0; idx < 20000; idx++)
+//     {
+//         i.idx_->add(idx);
+//     }
 
-    for (size_t idx = 0; idx < 2000; idx++)
-    {
-        uint32_t len = i.idx_.get_len(idx);
-        if (len != idx)
-        {
-            cout<<"idx = "<<idx<<" len = "<<len<<endl;
-        }
+//     for (size_t idx = 0; idx < 20000; idx++)
+//     {
+//         uint32_t len = i.idx_->get_len(idx);
+//         if (len != idx)
+//         {
+//             cout<<"idx = "<<idx<<" len = "<<len<<endl;
+//             exit(0);
+//         }
         
-    }
+//     }
 
+//     i.idx_->release();
+
+//     ctx.init_type_ = io_init_type::init_exist;
+//     io i2;
+//     i2.init(ctx);
+//     for (size_t idx = 0; idx < 20000; idx++)
+//     {
+//         uint32_t len = i2.idx_->get_len(idx);
+//         if (len != idx)
+//         {
+//             cout<<"idx = "<<idx<<" len = "<<len<<endl;
+//             exit(0);
+//         }
+        
+//     }
+
+// }
+
+
+static uint64_t get_cnt(uint64_t idx)
+{
+    return idx % 50+10;
 }
 
+static void write_iov2(iovec *iov,uint64_t val)
+{
+    uint64_t *p = (uint64_t *)iov->iov_base;
+    for (int j = 0; j < get_cnt(val); j++)
+    {
+        p[j] = val;
+    }
+    iov->iov_len = get_cnt(val)*sizeof(uint64_t);
+}
+
+static void read_iov2(iovec *iov,uint64_t val)
+{
+    if(iov->iov_len != get_cnt(val)*sizeof(uint64_t))
+    {
+        printf("iov_len=%lu,ok=%lu\n",iov->iov_len,get_cnt(val)*sizeof(uint64_t));
+    }
+    uint64_t *p = (uint64_t *)iov->iov_base;
+    for (int j = 0; j < get_cnt(val); j++)
+    {
+        if(p[j] != val)
+            printf("read_iov error val = %ju\n",val);
+    }
+}
+
+
+static int io_idx_test_w()
+{
+    io_meta m;
+    m.blk_cnt_max_ = 256;
+    m.blk_size_ = 1024;
+    m.io_type_ = 0;
+
+    io_context ctx;
+    ctx.meta_ = m;
+    ctx.path_ = "./io_save/";
+    ctx.prefix_ = "io_pre";
+    ctx.rw_type_ = io_rw_type::rw_write;
+    ctx.init_type_ = io_init_type::init_new;
+
+    io iow;
+    auto ret = iow.init(ctx);
+    CHECK0_RETV(ret,0);
+
+
+    rb_iov rb;
+    rb.init(1024*1024,1024);
+
+    uint64_t cnt = 1024*1024;
+    iovec *iov = rb.iov_;
+    for(int i = 0; i < cnt; i++)
+    {
+        write_iov2(iov+i,i);
+
+    }
+
+    uint64_t start = 0;
+    while(cnt > 0)
+    {
+        uint32_t written = 0;
+        int wret = iow.write_data(iov+start,cnt,written);
+        printf("written = %u\n",written);
+        printf("wret = %d\n",wret);
+
+        if (wret == 0)
+        {
+            start += written;
+            cnt -= written;
+        }
+        else
+        {
+            printf("wret = %d\n",wret);
+            exit(0);
+        }
+    }
+}
+
+
+
+
+
+static int io_idx_test_r()
+{
+    io_meta m;
+    m.blk_cnt_max_ = 256;
+    m.blk_size_ = 1024;
+    m.io_type_ = 0;
+
+    io_context ctx;
+    ctx.meta_ = m;
+    ctx.path_ = "./io_save/";
+    ctx.prefix_ = "io_pre";
+    ctx.rw_type_ = io_rw_type::rw_read;
+
+    // read
+    io ior;
+    auto ret = ior.init(ctx);
+    
+    rb_iov rb;
+    rb.init(1024*1024,1024);
+
+    uint64_t cnt = 1024*1024;
+    iovec *iov = rb.iov_;
+
+
+    uint64_t start = 0;
+
+    while(cnt > 0)
+    {
+        uint32_t readed = 0;
+        int rret = ior.read_data(iov+start,cnt,start,readed);
+        printf("readed = %u\n",readed);
+        printf("rret = %d\n",rret);
+        if (rret == 0)
+        {
+            start += readed;
+            cnt -= readed;
+        }
+        else
+        {
+            printf("rret = %d\n",rret);
+            exit(0);
+        }
+    }
+
+    for(int i = 0; i < 1024*1024; i++)
+    {
+        iovec *cur = iov+i;
+        read_iov2(cur,i);
+    }
+
+    printf("check data ok\n");
+
+    return 0;
+}
+static int io_test_data_ok()
+{
+    io_idx_test_w();
+    io_idx_test_r();
+
+}
 static void io_test()
 {
-    io_test2();
-
+    //io_test2();
+    io_test_data_ok();
 
 
 }
