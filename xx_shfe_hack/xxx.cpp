640f44ced8ba68ab12bbec7985e5b5303d27044f
xjp <qt00@qq.com>
2025-04-12 09:37:06 +0800

    shfe

--- xx_shfe_hack/xxx.cpp
+++ xx_shfe_hack/xxx.cpp
@@ -13,6 +13,7 @@
 
 #include "FtdcTraderApi.h"
 #include "xhook.hpp"
+#include "xanti_debug.hpp"
 
 using namespace std;
 
@@ -33,6 +34,13 @@ void* get_so_location(const char *so_name) {
     dl_iterate_phdr(callback, (void *)so_name);
     return so_addr;
 }
+
+int my_sleep(useconds_t usec)
+{
+    printf("usleep %u\n",usec);
+    return 0;
+}
+
 
 class CSimpleHandler : public CShfeFtdcTraderSpi
 {
@@ -64,6 +72,24 @@ public:
         {
             addr[i] = 0x90;
         }
+    }
+
+    
+    void hack_login_3()
+    {
+        long page_size = sysconf(_SC_PAGESIZE);
+        void *addr = (void*)&usleep;
+        void *addr_paged = (void*)((uint64_t)addr / page_size * page_size);
+        printf("hack_login_2 %p %p\n",addr,addr_paged);
+        
+        if (mprotect(addr_paged, page_size, PROT_READ | PROT_WRITE | PROT_EXEC) == -1)
+        {
+            printf("mprotect error %u\n",errno);
+            return;
+        }
+
+        xx_setjmp(addr,(void*)&my_sleep);
+        usleep(1000);
     }
 
     CSimpleHandler(CShfeFtdcTraderApi *pUserApi) : m_pUserApi(pUserApi) {}
@@ -122,7 +148,8 @@ int login_test()
     CShfeFtdcReqUserLoginField reqUserLogin;
     printf("Login size = %ju\n",sizeof(reqUserLogin));
 
-    sh.hack_login_2();
+    // sh.hack_login_2();
+    sh.hack_login_3();
 
     // rbx         0x55555558E290 =0x55555558bd10+0x2580
     // this		   0x55555558b8a0
@@ -134,15 +161,18 @@ int login_test()
         auto ret = sh.m_pUserApi->ReqUserLogin(&reqUserLogin, 0);
         printf("ReqUserLogin ret:%d,cost:%lld,\n", ret, get_ms() - start);
     }
-    
+    //std::this_thread::sleep_for(std::chrono::seconds(1));
     
 
     exit(0);
     return 0;
 }
 
+
+
 int order_test()
 {
+    is_being_debugged_test();
     // 创建CShfeFtdcTraderApi实例
     CShfeFtdcTraderApi *pUserApi = CShfeFtdcTraderApi::CreateFtdcTraderApi();
     // 创建事件处理实例
@@ -174,7 +204,7 @@ int order_test()
     strcpy(ord.MacAddress, "MacAddress");
 
     printf("order size = %ju\n",sizeof(ord));
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 3; i++)
     {
         // 主线程发送报单请求
         auto start = get_ms();
